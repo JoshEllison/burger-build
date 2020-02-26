6a4737eccd604cae644f4f0cf5adc7d551ad16b3
docholliday211 <35431728+docholliday211@users.noreply.github.com>
2020-02-26 14:54:15 -0800

    login and logout routing complete

--- src/hiorderc/Layout/Layout.js
+++ src/hiorderc/Layout/Layout.js
@@ -1,37 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Auxiliary from '../Auxiliary/Auxiliary';
-import classes from './Layout.css';
-import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
-import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+import Auxiliary from "../Auxiliary/Auxiliary";
+import classes from "./Layout.css";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
+import { connect } from "react-redux";
 
 class Layout extends Component {
-    state = {
-        showSideDrawer: true
-    }
+  state = {
+    showSideDrawer: true
+  };
 
-    sideDrawerClosedHandler = () => {
-        this.setState({showSideDrawer: false});
-    }
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
 
-    sideDrawerToggleHandler = () => {
-        this.setState((prevState) => {
-            return {showSideDrawer: !prevState.showSideDrawer};
-        });
-    }
-    
-    render () {
-        return (
-            <Auxiliary>
-                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
-                <SideDrawer 
-                    open={this.state.showSideDrawer}
-                    closed={this.sideDrawerClosedHandler} />
-                <main className={classes.Content}>
-                    {this.props.children}
-                </main>
-            </Auxiliary>
-        )
-    }
+  sideDrawerToggleHandler = () => {
+    this.setState(prevState => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
+
+  render() {
+    return (
+      <Auxiliary>
+        <Toolbar
+          isAuth={this.props.isAuthenticated}
+          drawerToggleClicked={this.sideDrawerToggleHandler}
+        />
+        <SideDrawer
+          isAuth={this.props.isAuthenticated}
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Auxiliary>
+    );
+  }
 }
-export default Layout;
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
+export default connect(mapStateToProps)(Layout);
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -23,11 +23,11 @@ export const purchaseBurgerStart = () => {
 };
 
 // async
-export const purchaseBurger = orderData => {
+export const purchaseBurger = (orderData, token) => {
   return dispatch => {
     dispatch(purchaseBurgerStart());
     axios
-      .post("/orders.json", orderData)
+      .post("/orders.json", token, orderData)
       .then(response => {
         console.log(response.data);
 
@@ -66,11 +66,11 @@ export const fetchOrdersStart = () => {
 };
 
 // async
-export const fetchOrders = () => {
+export const fetchOrders = token => {
   return dispatch => {
     dispatch(fetchOrdersStart());
     axios
-      .get("/orders.json")
+      .get("/orders.json?auth=" + token)
       .then(res => {
         const fetchedOrders = [];
         for (let key in res.data) {
